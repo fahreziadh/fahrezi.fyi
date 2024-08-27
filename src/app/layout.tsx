0d64d8d1d80c05a0d5f44570a1075e04231a698c
Fahrezi <fahreziadh@gmail.com>
2024-08-28 02:25:48 +0700

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,31 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import { GeistSans } from 'geist/font/sans';
+import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
   title: "Fahrezi Adha",
   description: "Frontend Engineer",
+  openGraph: {
+    type: "website",
+    url: "https://fahrezi.fyi",
+    description: "Frontend Engineer",
+    title: "Fahrezi Adha",
+    siteName: "Fahrezi Adha",
+    images: [
+      {
+        url: "https://fahrezi.fyi/og_image.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@fahreziadhaa",
+    title: "Fahrezi Adha",
+    description: "Frontend Engineer",
+    images: "https://fahrezi.fyi/og_image.png",
+    creator: "@fahreziadhaa",
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`dark bg-background text-foreground font-sans antialiased ${GeistSans.variable}`}>{children}</body>
+      <body
+        className={`dark bg-background text-foreground font-sans antialiased ${GeistSans.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
